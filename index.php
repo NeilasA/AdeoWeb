b2b118b773af909715ec8a2c344cb87a1a034804
Neilas <neilas.armonas@gmail.com>
2021-03-07 18:50:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,9 +12,15 @@
 
         $city_name = "kaunas";
         $api_url='https://api.meteo.lt/v1/places/'.$city_name.'/forecasts/long-term';
-        $wheather_data = file_get_contents($api_url);
+        $wheather_data = json_decode( file_get_contents($api_url), true);
+        $weather_forcast_day_1 = $wheather_data['forecastTimestamps']['0']['conditionCode'];
+        $weather_forcast_day_2 = $wheather_data['forecastTimestamps']['24']['conditionCode'];
+        $weather_forcast_day_3 = $wheather_data['forecastTimestamps']['48']['conditionCode'];
         echo"<pre>";
-        print_r($wheather_data);
+        print_r($weather_forcast_day_1);
+        print_r($weather_forcast_day_2);
+        print_r($weather_forcast_day_3);
+        print_r($weather_forcast_data);
     
     ?>
    
